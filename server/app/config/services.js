d9fc5283db501b8af6432169a3b5fae7cecde9ae
whosesmile <lovemoon@yeah.net>
2015-08-05 16:49:51 +0800

    修复上传BUG

--- server/app/config/services.js
+++ server/app/config/services.js
@@ -62,8 +62,8 @@ app.factory('commonService', function ($q, $http, $injector, $modal, growl) {
         url: '/web/car/importCarModelExcel',
         method: 'POST',
         file: file
-      }).then(function (res) {
-        deferred.resolve(res.entity);
+      }).then(function (data) {
+        deferred.resolve(data);
         growl.addSuccessMessage('上传成功');
       }, function (rej) {
         growl.addErrorMessage(rej.message || '上传失败');
