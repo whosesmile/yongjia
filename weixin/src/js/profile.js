6851638349de7cf56377d5e2f9785291db895d78
whosesmile <lovemoon@yeah.net>
2015-08-24 19:40:29 +0800

    验证码提交

--- weixin/src/js/profile.js
+++ weixin/src/js/profile.js
@@ -3,27 +3,43 @@ $(function () {
   if (!$('body').is('.settings')) {
     return false;
   }
+
   var mobile = $('input[name="mobile"]').val().trim();
 
+  // 绑定行为
   $('.settings .captcha .action').on('click', function () {
     var button = $(this);
-    var mobile = $('input[name="mobile"]').val().trim();
-    if (/^\d{11}$/.test(mobile)) {
-      $.post('/wx/sendsms', {
-        mobile: mobile,
-        type: 0
-      }).then(function (res) {
-        if (res.code === 200) {
-          notice('验证码发送成功');
-        }
-        else {
-          notice('验证码发送失败');
-        }
-      });
+
+    var newMobile = $('input[name="mobile"]').val().trim();
+
+    if (newMobile === '') {
+      return notice.warning("请输入合法的手机号");
     }
-    else {
-      notice('请输入合法的手机号');
-    }
+
+    // 验证码
+    var element = $(this).addClass('timer');
+    var remaining = 59;
+    interval = setInterval(function () {
+      element.text(remaining);
+      remaining -= 1;
+      if (remaining == -1) {
+        clearInterval(interval);
+        element.removeClass('timer').text('发送验证码');
+      }
+    }, 1000);
+    element.text(remaining + 1);
+
+    $.post('/wx/sendsms', {
+      mobile: newMobile,
+      type: 0
+    }).then(function (res) {
+      if (res.code === 200) {
+        notice('验证码发送成功');
+      }
+      else {
+        notice(res.data.message);
+      }
+    });
   });
 
   $('.settings input[name="mobile"]').on('input', function () {
